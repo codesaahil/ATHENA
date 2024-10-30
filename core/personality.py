fd2aa476b2e097a4de836141c48292fc268c9ab5
codesaahil <mahatosaahil4@gmail.com>
2024-10-30 17:30:42 +0545

    feat: personality data class

---
+++ core/personality.py
@@ -0,0 +1,12 @@
+from dataclasses import dataclass
+
+from core.emotional_state import EmotionalState
+from core.ocean_profile import OceanProfile
+
+
+@dataclass
+class Personality:
+    emotionalState: EmotionalState
+    oceanProfile: OceanProfile
+
+
